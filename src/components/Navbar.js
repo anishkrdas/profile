9b79342742b294e82b0b4a3c9cda09723db9408d
anishkrdas <anishkrdas@outlook.com>
2021-10-01 23:46:10 +0530

    Profile: 5.1 - image borders removed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -16,13 +16,13 @@ const Navbar = () => {
   <div className="collapse navbar-collapse" id="navbarSupportedContent">
     <ul className="navbar-nav mr-auto">
       <li className="nav-item active">
-        <Link smooth={true} to="about" spy={true} offset={-50} duration={1} className="nav-link" href="#">About<span className="sr-only">(current)</span></Link>
+        <Link smooth={true} to="about" spy={true} offset={-30} duration={1} className="nav-link" href="#">About<span className="sr-only">(current)</span></Link>
       </li>
       <li className="nav-item">
-        <Link smooth={true} spy={true} to="journey" offset={-50} duration={1} className="nav-link" href="#">Profile</Link>
+        <Link smooth={true} spy={true} to="journey" offset={-30} duration={1} className="nav-link" href="#">Profile</Link>
       </li>
       <li className="nav-item">
-        <Link smooth={true} to="techs" spy={true} offset={-50} duration={1} className="nav-link" href="#">Technologies</Link>
+        <Link smooth={true} to="techs" spy={true} offset={-30} duration={1} className="nav-link" href="#">Technologies</Link>
       </li>
       {/*<li className="nav-item">
         <Link smooth={true} to="whatIDo" offset={-50} className="nav-link" href="#">Grinds</Link>
@@ -38,7 +38,7 @@ const Navbar = () => {
         </div>
     </li>*/}
       <li className="nav-item">
-        <Link smooth={true} to="contact" spy={true} offset={-50} duration={1} className="nav-link" href="#">Contact</Link>
+        <Link smooth={true} to="contact" spy={true} offset={-30} duration={1} className="nav-link" href="#">Contact</Link>
       </li>
 
     </ul>
