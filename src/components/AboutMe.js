656462486c16d728172d7ec1d25e55075e8ca349
anishkrdas <anishkrdas@outlook.com>
2022-06-28 23:24:03 +0530

    update in css

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -31,15 +31,15 @@ const AboutMe = () => {
                   <h1 className="about-story">MY STORY</h1>                  
                 <div className="about-me-dets">
                 <p className="imageDesc">An active man of principles, punctuality and determination with the exact proportionality of self-confidence, optimism and respect for others and self, alongside, being adaptive in nature to any change at any point of time.</p>
-                    <p>Focused and smart Software Development Team Leader; is able to {/*motivate developmentteam and */}identify and remove impediments to turning out quality software applications for customers. Excellent ability to read code and suggest changes to make the finished project glitch-free in addition to holding the below qualities &ndash;</p>
+                    <p>Focused and smart Software Development Team Leader; is able to {/*motivate developmentteam and */}identify and remove impediments alongside guiding the team to deliver quality software applications for enterprise customers. Excellent ability to read code and suggest changes to make the finished project glitch-free in addition to holding the below qualities &ndash;</p>
                     <p>
                         <ul>
-                           {/* <li>Consistently delivering software that meets standards</li>*/}
                             <li>Ensuring, the programming team develops high-quality working applications</li>
                             <li>Prioritizing programming work and assigning tasks</li>
-                            <li>Appropriately dealing with problems in the team</li>
-                            <li>Monitoring timeliness of attendance at team meetings</li>
-                            <li>Efficiently executing assigned actions</li>
+                            <li>Consistently delivering digital software that meets standards</li>
+                            <li>Appropriately dealing with problems and managing the morale of the team</li>
+                            <li>Monitoring timeliness of attendance and deliverance</li>
+                            <li>Efficiently executing assigned actions and suggestive corrective measures at sprint retrospectives</li>
                         </ul>
 
                     </p>
